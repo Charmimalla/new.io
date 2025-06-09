3ce078c7f044596f76449e6234467147ed40acf2
Charmimalla <charminimalla03@gmail.com>
2025-06-09 12:20:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth scroll nav
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for reaching out, Charmini will contact you soon!');
+    this.reset();
+  });
+  
